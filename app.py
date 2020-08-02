c43cab17444c6ff647c88274bb6190e996c347fb
Yumiko Miyamoto <miyumi1018@gmail.com>
2020-08-02 21:34:10 +0900

    プロフィール画像追加まで完了

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import os
+# ファイル名をチェックする関数
+from werkzeug.utils import secure_filename
+# 画像のダウンロード
+from flask import send_from_directory
 # splite3をimportする
 import sqlite3
 # flaskをimportしてflaskを使えるようにする
-from flask import Flask , render_template , request , redirect , session
+from flask import Flask , render_template , request , redirect , url_for, send_from_directory, session
 
 # fromでstatistics.pyというモジュール(ファイル)を読み込む、importでstatistics.py内のmean()という関数を読み込む
 import statistics
@@ -15,6 +20,15 @@ app = Flask(__name__)
 
 # Flask では標準で Flask.secret_key を設定すると、sessionを使うことができます。この時、Flask では session の内容を署名付きで Cookie に保存します。
 app.secret_key = 'sunabakoza'
+
+UPLOAD_FOLDER = './static/img'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 def index():
@@ -36,12 +50,26 @@ def register():
         name = request.form.get("name")
         password = request.form.get("password")
 
-        conn = sqlite3.connect('service.db')
-        c = conn.cursor()
-        c.execute("insert into user values(null,?,?)", (name,password))
-        conn.commit()
-        conn.close()
-        return redirect('/login')
+        #画像
+        img_file = request.files['img_file']
+        if img_file and allowed_file(img_file.filename):
+            filename = secure_filename(img_file.filename)
+            img_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            imgdata = '/static/img/' + filename
+            conn = sqlite3.connect('service.db')
+            c = conn.cursor()
+            c.execute("insert into user values(null,?,?,?)", (name,password,imgdata))
+            conn.commit()
+            conn.close()
+            # return redirect('/login')
+            return render_template('login.html', img_url=imgdata)
+        else:
+            return ''' <p>許可されていない拡張子です</p> '''
+
+@app.route('/static/img/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    
 
 
 # GET  /login => ログイン画面を表示
@@ -97,11 +125,11 @@ def bbs():
         c = conn.cursor()
         # # DBにアクセスしてログインしているユーザ名と投稿内容を取得する
         # クッキーから取得したuser_idを使用してuserテーブルのnameを取得
-        c.execute("select name from user where id = ?", (user_id,))
+        c.execute("select name,imgdata from user where id = ?", (user_id,))
         # fetchoneはタプル型
         user_info = c.fetchone()
-        c.execute("select id,comment,postdate from bbs where userid = ? order by id", (user_id,))
 
+        c.execute("select id,comment,postdate from bbs where userid = ? order by id", (user_id,))
         comment_list = []
         for row in c.fetchall():
             comment_list.append({"id": row[0], "comment": row[1], "postdate": row[2]})
@@ -201,6 +229,7 @@ def del_task():
     return redirect("/bbs")
 
 
+
 @app.errorhandler(403)
 def mistake403(code):
     return 'There is a mistake in your url!'
@@ -209,6 +238,8 @@ def mistake403(code):
 @app.errorhandler(404)
 def notfound404(code):
     return "404だよ！！見つからないよ！！！"
+
+
 
 
 # __name__ というのは、自動的に定義される変数で、現在のファイル(モジュール)名が入ります。 ファイルをスクリプトとして直接実行した場合、 __name__ は __main__ になります。
